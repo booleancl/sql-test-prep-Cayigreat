e4a27199c358514e0de41279b027ab1dbef1a162
cayigreat <cayiarja@gmail.com>
2022-10-21 11:14:03 -0300

    sql test

---
+++ migrations/05-seed_data.sql
@@ -0,0 +1,6 @@
+\c ecommerce
+
+\COPY customers FROM 'C:/Users/javil/intro_dev/sql-test-prep-Cayigreat/data/customers.csv'csv HEADER;
+\COPY products FROM 'C:/Users/javil/intro_dev/sql-test-prep-Cayigreat/data/products.csv'csv HEADER;
+\COPY details FROM 'C:/Users/javil/intro_dev/sql-test-prep-Cayigreat/data/purchase_details.csv'csv HEADER;
+\COPY purchases FROM 'C:/Users/javil/intro_dev/sql-test-prep-Cayigreat/data/purchases.csv'csv HEADER;
